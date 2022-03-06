cb17a4ebd2a0a1c8f3e8409716cbdf3bec1ae13e
Michal Mojžiš <missko111@gmail.com>
2022-03-06 16:19:00 +0100

    Fix import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from "@angular/router";
 import {KnihyStrankaComponent} from "./knihy-stranka/knihy-stranka.component";
+import {CustomerStrankaComponent} from "./customer-stranka/customer-stranka.component";
 
 const routes: Routes = [
   {
