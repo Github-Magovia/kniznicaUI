c02292fd5341e1da13628e082fbce707d2fdd141
Michal Mojžiš <86914951+misko3001@users.noreply.github.com>
2022-03-06 16:38:24 +0100

    Revert "Vypozicky - komponenty + prepojenie"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,9 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MatButtonModule} from "@angular/material/button";
-import {AppRoutingModule} from "./app-routing.module";
-import { VypozickyStrankaComponent } from './vypozicky-stranka/vypozicky-stranka.component';
-import { VypozickyFormularComponent } from './vypozicky-formular/vypozicky-formular.component';
-import { VypozickyZoznamComponent } from './vypozicky-zoznam/vypozicky-zoznam.component';
 @NgModule({
   declarations: [
-    AppComponent,
-    VypozickyStrankaComponent,
-    VypozickyFormularComponent,
-    VypozickyZoznamComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -22,8 +15,7 @@ import { VypozickyZoznamComponent } from './vypozicky-zoznam/vypozicky-zoznam.co
     NgbModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    MatButtonModule,
-    AppRoutingModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
