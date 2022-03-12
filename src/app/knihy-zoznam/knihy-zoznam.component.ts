cb78815acab5e434b49c8af1c45f8353646f4036
ppalo21 <ppavlik48@gmail.com>
2022-03-12 11:02:15 +0100

    knihaZoznam

--- src/app/knihy-zoznam/knihy-zoznam.component.ts
+++ src/app/knihy-zoznam/knihy-zoznam.component.ts
@@ -1,17 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input,} from '@angular/core';
 import {Kniha} from "../models/kniha.model";
+import {Output} from "@angular/core";
 
 @Component({
   selector: 'app-knihy-zoznam',
   templateUrl: './knihy-zoznam.component.html',
   styleUrls: ['./knihy-zoznam.component.css']
 })
-export class KnihyZoznamComponent implements OnInit {
-  @Input()knihy: Kniha[] = [];
+export class KnihyZoznamComponent {
+  @Input()
+  knihy: Kniha[] = [];
+
+  @Output()
+  upravKnihu: EventEmitter<Kniha> = new EventEmitter<Kniha>();
+
+  @Output()
+  zmazKnihu: EventEmitter<Kniha> = new EventEmitter<Kniha>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  uprav(kniha: Kniha[]): void {
+    this.upravKnihu.emit(kniha);
   }
+
+  zmaz(kniha: Kniha[]): void {
+    this.zmazKnihu.emit(kniha);
+  }
+
 
 }
