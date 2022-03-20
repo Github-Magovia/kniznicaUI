d0d89ba6bd6fa9c2bec3aa4285556b6dc585dfb9
Michal Mojžiš <missko111@gmail.com>
2022-03-20 10:52:14 +0100

    Implementacia knihy service (podla REST)

--- src/services/knihy/knihy.service.ts
+++ src/services/knihy/knihy.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Kniha} from "../../app/models/kniha.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class KnihyService {
+  private apiUrl = 'http://localhost:8080/api/books';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public createBook(k: Kniha): Observable<Kniha> { return this.http.post<Kniha>(`${this.apiUrl}`, k); }
+
+  public getAllBooks(): Observable<Kniha[]> { return this.http.get<Kniha[]>(`${this.apiUrl}`); }
+
+  public getBookById(knihaId: number): Observable<Kniha> {
+    return this.http.get<Kniha>(`${this.apiUrl}/${knihaId}`);
+  }
+
+  public updateBook(knihaId: number, k: Kniha) { return this.http.put<Kniha>(`${this.apiUrl}/${knihaId}`, k); }
+
+  public deleteBook(knihaId: number): void { this.http.delete(`${this.apiUrl}/${knihaId}`) }
 }
