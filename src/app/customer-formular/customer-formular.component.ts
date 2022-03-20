f116bf95719c85119825249d8b4116f0b04fe89a
ppalo21 <ppavlik48@gmail.com>
2022-03-20 12:54:28 +0100

    upravenie customer-formular podla CustomerDto

--- src/app/customer-formular/customer-formular.component.ts
+++ src/app/customer-formular/customer-formular.component.ts
@@ -1,22 +1,64 @@
-import {Component, EventEmitter, OnInit, Output} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {Customer} from "../models/customer.model";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-customer-formular',
   templateUrl: './customer-formular.component.html',
   styleUrls: ['./customer-formular.component.css']
 })
-export class CustomerFormularComponent implements OnInit{
-  customer: Customer = {id: 0,meno: 'dadasd', contact: 'example@gmail.com'}
-  @Output() customerEmitter = new EventEmitter<Customer>();
-  constructor() {}
+export class CustomerFormularComponent {
+  // customer: Customer = {id: 0,meno: 'dadasd', contact: 'example@gmail.com'}
 
-  ngOnInit(): void {
+  @Input()
+  set customer(data: Customer) {
+    if (data) {
+      this.naplnFormular(data);
+    }
+  }
+  @Output()
+  pridajOsobuEmitter = new EventEmitter<Customer>();
+  @Output()
+  upravOsobuEmitter = new EventEmitter<Customer>();
 
+  customerForm: FormGroup;
+
+  constructor() {
+    this.vytvorFormular();
   }
 
-  public pridajCustomera() {
-    this.customerEmitter.emit(this.customer);
+  private vytvorFormular(): void {
+    this.customerForm = new FormGroup({
+      id: new FormControl(null),
+      customerName: new FormControl(null),
+      email: new FormControl(null)
+    });
+  }
+  private naplnFormular(cust: Customer): void {
+    this.customerForm.controls['id'].setValue(cust.id);
+    this.customerForm.controls['customerName'].setValue(cust.customerName);
+    this.customerForm.controls['email'].setValue(cust.email);
+  }
+  public pridaj(): void {
+    this.pridajOsobuEmitter.emit({
+      id: Math.random().toString(),
+      customerName: this.customerForm.value.meno,
+      email: this.customerForm.value.contact
+
+    });
+    this.customerForm.reset();
+  }
+  public uprav(): void{
+    this.upravOsobuEmitter.emit(this.customerForm.value);
+    this.customerForm.reset();
+  }
+
+  public odstran(): void {
+    this.customer = undefined;
+    this.customerForm.reset();
+  }
+  ngOnInit(): void{
+
   }
 
 }
