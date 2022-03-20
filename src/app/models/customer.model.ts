577503b30d9f43ad505b5338daa0529193c410c4
ppalo21 <ppavlik48@gmail.com>
2022-03-20 14:33:01 +0100

    pridané aj firstName,lastName

--- src/app/models/customer.model.ts
+++ src/app/models/customer.model.ts
@@ -1,26 +1,18 @@
 export class Customer{
 
-  id?: String;
-  customerName: String;
-  email: String;
+  id?: number;
+  customerName?: String;
+  firstName: String;
+  lastName: String;
+  contact: String;
 
 
   constructor(customer: Customer) {
     this.id = customer.id;
     this.customerName = customer.customerName;
-    this.email = customer.email;
-  }
-}
-
-export class CustomerZoznamComponent{
-  id?: String;
-  meno: String;
-  contact: String;
-
-
-  constructor(customer: CustomerZoznamComponent) {
-    this.id = customer.id;
-    this.meno = customer.meno;
+    this.firstName = customer.firstName;
+    this.lastName = customer.lastName;
     this.contact = customer.contact;
+
   }
 }
