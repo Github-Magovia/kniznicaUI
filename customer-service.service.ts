099da4acb28d4f56501e0fbc4b01af4500db1994
ppalo21 <ppavlik48@gmail.com>
2022-03-20 14:54:26 +0100

    zmena id: na number

--- customer-service.service.ts
+++ customer-service.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Customer, CustomerZoznamComponent} from "./src/app/models/customer.model";
+import {Customer} from "./src/app/models/customer.model";
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +12,22 @@ export class CustomerServiceService {
 
   constructor(private http: HttpClient) { }
 
-  getCustomers(): Observable<CustomerZoznamComponent[]> {
-    return this.http.get<CustomerZoznamComponent[]>(`${this.apiUrl}`);
+  getCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${this.apiUrl}`);
   }
 
-  getCustomer(customerId: string): Observable<Customer> {
+  getCustomer(customerId: number): Observable<Customer> {
     return this.http.get<Customer>(`${this.apiUrl}/${customerId}`);
   }
 
   createCustomer(customer: Customer): Observable<Customer> {
     return this.http.post<Customer>(`${this.apiUrl}`, customer);
   }
-  updateCustomer(customerId: string, customer: Customer): Observable<Customer>{
+  updateCustomer(customerId: number, customer: Customer): Observable<Customer>{
     return this.http.put<Customer>(`${this.apiUrl}/${customerId}`, customer);
   }
 
-  deleteCustomer(customerId: string): Observable<void>{
+  deleteCustomer(customerId: number): Observable<void>{
     return this.http.delete<void>(`${this.apiUrl}/${customerId}`);
   }
 
